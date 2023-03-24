6f7dd63937777354fbb3a785b427b1848f285d1b
Francisco J Gutierrez Rodarte <Francisco.GutierrezRodarte@du.edu>
2023-03-23 18:23:20 -0700

    updated index file model

--- models/index.js
+++ models/index.js
@@ -4,30 +4,33 @@ const Category = require("./Category");
 const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
-// Products belongsTo Category
+// products many association to category
 Product.belongsTo(Category, {
   foreignKey: "category_id",
+  onDelete: "SET NULL",
 });
-// Categories have many Products
+// category many association to products
 Category.hasMany(Product, {
   foreignKey: "category_id",
+  onDelete: "SET NULL",
 });
-// Products belongToMany Tags (through ProductTag)
+// many to many relationship of product and tags through ProductTag
 Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
     unique: false,
   },
-  as: "products_tags",
+  as: "product_tags",
 });
-// Tags belongToMany Products (through ProductTag)
+// many to many relationship of product and tags through ProductTag
 Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
     unique: false,
   },
-  as: "tags_products_tags",
+  as: "tag_products",
 });
+
 module.exports = {
   Product,
   Category,
